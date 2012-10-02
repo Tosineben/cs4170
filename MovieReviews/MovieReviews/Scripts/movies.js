6ae2cb271ef490ac40758ab2bd17006521c12d21
Alden Quimby <aldenquimby@gmail.com>
2012-10-02 00:11:40 -0400

    make things look nicer

--- MovieReviews/MovieReviews/Scripts/movies.js
+++ MovieReviews/MovieReviews/Scripts/movies.js
@@ -7,18 +7,35 @@
             var self = this;
             self.settings = settings;
 
-            $('#navSearchButton').click(function () {
-                $('#navSearchButton').button('loading');
-
-                var query = $('#navSearchQuery').val().trim().replace(/ /g, '+');
+            $('#mainSearch-button').click(function () {
+                var queryObj = $('#mainSearch-query');
+                
+                var query = queryObj.val().trim().replace(/ /g, '+');
+                var criticsPick = $('#mainSearch-criticsPick').is(':checked');
+                var start = moment($('#mainSearch-startDate').val());
+                var end = moment($('#mainSearch-endDate').val());
 
                 if (query == '') {
-                    // no search
+                    queryObj.parents('.control-group').addClass('error');
+                    queryObj.parents('.control-group').find('.help-inline').show();
                     return;
                 }
 
+                queryObj.parents('.control-group').removeClass('error');
+                queryObj.parents('.control-group').find('.help-inline').hide();
+
                 // no offset for initial query
                 self.search = { query: query, offset: 0 };
+
+                if (criticsPick) {
+                    self.search.criticsPick = "Y";
+                }
+
+                if (start && end) {
+                    var dateRange = start.format("YYYY-MM-DD") + ';' + end.format("YYYY-MM-DD");
+                    self.search.openDate = dateRange;
+                }
+
                 self.doSearch(); 
             });
 
@@ -41,18 +58,21 @@
             });
         },
 
-        doSearch: function(exactMatch) {
+        doSearch: function() {
             var self = this;
 
-            if (exactMatch) {
-                self.search.query = "'" + self.search.query + "'";
-            }
+            $('#navSearchButton').button('loading');
+            $('#mainSearch-button').button('loading');
 
-            var url = self.settings.getSearchUrl(self.search.query, self.search.offset, self.search.criticsPick, self.search.thousandBest, 
-                                                 self.search.dvd, self.search.reviewer, self.search.pubDate, self.search.openDate);
+            var url = self.settings.getSearchUrl(self.search.query, self.search.offset, self.search.criticsPick, self.search.openDate);
 
             var proxyUrl = "proxy.php?url=" + encodeURIComponent(url);
 
+            // disable page numbers to make it clear that we're in the middle of searching
+            $.each($('.page-number'), function(index, pageNumber) {
+                $(pageNumber).parent().addClass('disabled');
+            });
+            
             $.getJSON(proxyUrl, function (rawResponse) {
 
                 var resp = jQuery.parseJSON(rawResponse);
@@ -62,37 +82,41 @@
 
                 // reset search button now that search is complete
                 $('#navSearchButton').button('reset');
+                $('#mainSearch-button').button('reset');
 
                 if (resp && resp.status == "OK") {
-                    // scroll page to top of movie list, slowly
-                    var top = $('#movie-list-separator').position().top - 40;
-                    $("html, body").animate({ scrollTop: top }, "slow");
-                    
                     self.displayPageNumbers(resp.num_results, self.search.offset);
                     self.displaySearchResults(resp.results);
                     self.fixUpMovieHeights();
                 }
                 else {
-                    self.displayAlert('Looks like the New York Times Movie Reviews API might be down, please try again later.');
+                    self.displayAlert('Looks like the New York Times Movie Reviews API might be down, please try again later.', true);
                 }
             });
         },
         
-        displayAlert: function (alertMsg) {
-            $('#primaryContainer').mustache('alert', { message: alertMsg }, { method: 'prepend' });
+        displayAlert: function (alertMsg, scroll) {
+            $('#alertContainer').mustache('alert', { message: alertMsg }, { method: 'html' });
             $('.alert').alert();
             
-            // scroll page to top so user can see error message
-            $("html, body").animate({ scrollTop: 0 }, "slow");
+            if (scroll) {
+                // scroll page so user can see error
+                var top = $('#alertContainer').position().top - 20;
+                $("html, body").animate({ scrollTop: top }, "slow");
+            }
         },
 
         displaySearchResults: function(results){
             var self = this;
 
             if (!results || results.length == 0) {
-                self.displayAlert('Your search returned no results. Try searching for something less specific.');
+                self.displayAlert('Your search returned no results. Try searching for something less specific.', false);
                 return;
             }
+            
+            // scroll page to top of movie list, slowly
+            var top = $('#movie-list-separator').position().top;
+            $("html, body").animate({ scrollTop: top }, "slow");
 
             var topResult = self.getMovieForDisplay(results[0]);
 
@@ -121,23 +145,7 @@
                 show: false
             });
         },
-        
-        fixUpMovieHeights: function () {
 
-            var maxHeight = 0;
-
-            $('#movie-list').children('.row-fluid').children('.span3').children('.well').each(function(index, movie) {
-                var height = $(movie).height();
-                if (height > maxHeight) {
-                    maxHeight = height;
-                }
-            });
-
-            $('#movie-list').children('.row-fluid').children('.span3').children('.well').each(function(index, movie) {
-                $(movie).height(maxHeight);
-            });
-        },
-        
         displayPageNumbers: function (numResults, offset) {
             var moviesPerPage = 20;
             var pageList = $('#pagination-list');
@@ -158,7 +166,7 @@
             var prevPage = { offset: offset - moviesPerPage, numDisplay: '&larr; Prev', disabled: activePage == 0 };
             var nextPage = { offset: offset + moviesPerPage, numDisplay: 'Next &rarr;', disabled: activePage == numPages };
 
-            var pages = [ prevPage ];
+            var pages = [prevPage];
 
             for (var i = 0; i <= numPages; i++) {
                 var page = { offset: i * moviesPerPage, numDisplay: i + 1, active: i == activePage };
@@ -167,27 +175,49 @@
 
             pages.push(nextPage);
 
-            pageList.mustache('page-numbers', {pages:pages}, { method: 'html' });
+            pageList.mustache('page-numbers', { pages: pages }, { method: 'html' });
         },
 
+        fixUpMovieHeights: function () {
+            var maxHeight = 130;
+
+            $('#movie-list').find('.well').each(function(index, movie) {
+                var height = $(movie).height();
+                if (height > maxHeight) {
+                    maxHeight = height;
+                }
+            });
+
+            $('#movie-list').find('.well').each(function (index, movie) {
+                $(movie).height(maxHeight);
+            });
+        },
+        
         getMovieForDisplay: function (movie) {
-        return {
-            title: movie.display_title,
-            headline: movie.headline,
-            summary: movie.summary_short,
-            imgSrc: ((movie.multimedia || {}).resource || {}).src || 'http://www.worldimpex.com/images/no_image_available.gif',
-            reviewerName: movie.byline,
-            reviewPubDate: moment(movie.publication_date).format('MMM Do YYYY'),
-            openedInTheatresDate: movie.opening_date ? moment(movie.opening_date).format('MMM Do YYYY') : null,
-            dvdReleaseDate: movie.dvd_release_date ? moment(movie.dvd_release_date).format('MMM Do YYYY') : null,
-            isCriticsPick: movie.critics_pick == 1,
-            isThousandBest: movie.thousand_best == "1",
-            reviewSnippet: movie.capsule_review,
-            reviewUrl: movie.link.url,
-            movieId: movie.nyt_movie_id,
-            relatedUrls: movie.related_urls
-        };
-    },
+            var m = {
+                title: movie.display_title,
+                headline: movie.headline,
+                summary: movie.summary_short,
+                imgSrc: movie.multimedia ? movie.multimedia.resource.src : 'http://www.worldimpex.com/images/no_image_available.gif',
+                reviewerName: movie.byline,
+                reviewPubDate: moment(movie.publication_date).format('MMM Do YYYY'),
+                openedInTheatresDate: movie.opening_date ? moment(movie.opening_date).format('MMM Do YYYY') : null,
+                dvdReleaseDate: movie.dvd_release_date ? moment(movie.dvd_release_date).format('MMM Do YYYY') : null,
+                isCriticsPick: movie.critics_pick == 1,
+                reviewSnippet: movie.capsule_review,
+                reviewUrl: movie.link.url,
+                movieId: movie.nyt_movie_id,
+                relatedUrls: movie.related_urls,
+                anyUrls: (movie.related_urls || {}).length > 0
+            };
+
+            $.each(m.relatedUrls, function(index, relatedUrl) {
+                relatedUrl.linkText = relatedUrl.suggested_link_text.replace(' of ' + m.title, '')
+                                                                    .replace(' for ' + m.title, '');
+            });
+
+            return m;
+        },
 
         getBioForDisplay: function (biography) {
             return {
@@ -201,12 +231,10 @@
 })(adq.module('movieReviews'));
 
 
-
 //** up to 3 of the following FILTERS - omit for all
 //query - keywords; matches titles and indexed terms =wild+west or ='wild+west' for exact match
 //critics-pick (Y/N) 
 //thousand-best (Y/N) - 1000 best movies ever
-//dvd (Y/N) - released on dvd yet
 //reviewer - 'manohla-dargis'
 //publication-date - of review, start and end date: YYYY-MM-DD;YYYY-MM-DD
 //opening-date - of movie in NY, single date: YYYY-MM-DD
--- MovieReviews/README.txt
+++ MovieReviews/README.txt
@@ -1,2 +1,24 @@
-no_image_avail from http://www.worldimpex.com/images/no_image_available.gif
-data from http://developer.nytimes.com/docs/read/movie_reviews_api
+no_image_avail from 
+http://www.worldimpex.com/images/no_image_available.gif
+
+data from 
+http://developer.nytimes.com/docs/read/movie_reviews_api
+
+new york times image
+http://www.gamification.co/wp-content/uploads/2011/03/New-York-Times-Logo-300x194.png
+
+Error handling:
+If a user searches for nothing, make the search bar red to indicate that the field
+should be filled out. Also add help text telling the user what they did wrong.
+
+If the NYT api responds with an error, show an error dialog that the user can close
+explaining the issue and asking them to try again at another time.
+
+If the search returns no results, tell the user, instead of leaving them wondering.
+
+Status updates:
+When they click search, the button changes to disabled and say "searching..." to make
+it clear that the user should wait
+
+When a page number is clicked, all page numbers are disabled, to make it clear that
+the user should wait for the next page of information to load
