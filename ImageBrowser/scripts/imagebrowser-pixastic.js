db27bd2c3782dec314fe826df498a0e282dd8feb
Alden Quimby <aldenquimby@gmail.com>
2012-11-22 12:23:05 -0500

    add jcrop;

--- ImageBrowser/scripts/imagebrowser-pixastic.js
+++ ImageBrowser/scripts/imagebrowser-pixastic.js
@@ -1,10 +1,49 @@
 
 function rotate(imgObj, right) {
-    imgObj.pixastic("rotate", {
+    replaceCanvas(imgObj.pixastic("rotate", {
         angle: right ? 90 : -90
-    });
+    }));
 }
 
 function invert(imgObj) {
-    imgObj.pixastic("invert");
+    replaceCanvas(imgObj.pixastic("invert"));
+}
+
+function undo(imgObj) {
+	replaceCanvas(imgObj.pixastic("revert"));
+}
+
+function sepia(imgObj) {
+	replaceCanvas(imgObj.pixastic("revert"));
+}
+
+function invert2() {
+	var img = new Image();
+	img.onload = function() {
+		Pixastic.process(img, "invert");
+	}
+	document.body.appendChild(img);
+	img.src = "myimage.jpg";
+}
+
+function replaceCanvas(pixasticResult) {
+	_ctx.drawImage(pixasticResult[0], 0, 0);
+}
+
+function crop() {
+	var canvas = $("#myCanvas");
+	canvas.Jcrop({
+	    onSelect: function (coords) {
+	        canvas.pixastic("crop", {
+	            rect: {
+	                left: coords.x,
+	                top: coords.y,
+	                width: coords.w,
+	                height: coords.h
+	            }
+	        });
+	        this.release();
+	        this.disable();
+	    }
+	});
 }
